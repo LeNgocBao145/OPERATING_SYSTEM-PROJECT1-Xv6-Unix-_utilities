f3accaebd3cdbfbab9512fe6be6f206e3eb60e4d
LeNgocBao145 <himagito@gmail.com>
2025-03-07 20:11:50 +0700

    Add a check for creating pipes Close pipes when do not need to read or write to prevent resource leak Add check when read or write on a pipe

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,8 +5,11 @@ int main() {
     char buf;
     int pid;
 
-    pipe(p1);
-    pipe(p2);
+    // Checking when creating pipes
+    if (pipe(p1) == -1 || pipe(p2) == -1) {
+        printf("Pipe creation failed\n");
+        exit(1);
+    }
 
     pid = fork();
     if (pid < 0) {
@@ -15,17 +18,41 @@ int main() {
     }
 
     if (pid == 0) {  // Child process
-        read(p1[0], &buf, 1);  // Read from parent
+        close(p1[1]);  // Do not need to write p1
+        close(p2[0]);  // Do not need to read p2
+
+        if (read(p1[0], &buf, 1) <= 0) { // Receive from parent
+            printf("Child read failed\n");
+            exit(1);
+        }
+
         printf("%d: received ping\n", getpid());
 
-        write(p2[1], &buf, 1); // Send back to parent
+        if (write(p2[1], &buf, 1) == -1) { // Send to parent
+            printf("Child write failed\n");
+            exit(1);
+        }
+
+        close(p1[0]);  // Close after reading p1
+        close(p2[1]);  // Close after writing p2
         exit(0);
     } else {  // Parent process
-        write(p1[1], "x", 1); // Send to child
+        close(p1[0]);  // Do not need to read p1
+        close(p2[1]);  // Do not need to write p2
 
-        read(p2[0], &buf, 1); // Receive from child
+        if (write(p1[1], "x", 1) == -1) { // Send to child
+            printf("Parent write failed\n");
+            exit(1);
+        }
+
+        if (read(p2[0], &buf, 1) <= 0) { // Receive from child
+            printf("Parent read failed\n");
+            exit(1);
+        }
         printf("%d: received pong\n", getpid());
 
+        close(p1[1]);  // Close after writing p1
+        close(p2[0]);  // Close after reading p2
         wait(0); // Wait for child to finish
     }
     exit(0);
